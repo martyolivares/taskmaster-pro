db91c6674ad086278350f6f1b1a183da2b7ad2a9
martyolivares <mar.olivares01@gmail.com>
2022-08-03 19:28:26 -0500

    Added ability to edit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,7 +33,7 @@ var loadTasks = function() {
 
   // loop over object properties
   $.each(tasks, function(list, arr) {
-    console.log(list, arr);
+    
     // then loop over sub-array
     arr.forEach(function(task) {
       createTask(task.text, task.date, list);
@@ -44,6 +44,18 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+    
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+
+    $(this).replaceWith(textInput);
+}); 
 
 
 
