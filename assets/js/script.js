d48dee9bf7a9c6416e91cbbf32336c6e1e24dfec
martyolivares <mar.olivares01@gmail.com>
2022-08-17 22:19:40 -0500

    Added ability to edit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ var loadTasks = function() {
       done: []
     };
   }
-
+// having to make comment right here
   // loop over object properties
   $.each(tasks, function(list, arr) {
     
@@ -55,8 +55,37 @@ $(".list-group").on("click", "p", function() {
     .val(text);
 
     $(this).replaceWith(textInput);
+    textInput.trigger("focus");
 }); 
 
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  tasks[status][index].text = text;
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // reaplace textarea with p element
+  $(this).replaceWith(taskP);
+});
 
 
 
@@ -71,7 +100,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
   // highlight textarea
   $("#modalTaskDescription").trigger("focus");
 });
-
+// this is content that is going to be deleted
 // save button in modal was clicked
 $("#task-form-modal .btn-primary").click(function() {
   // get form values
@@ -106,4 +135,53 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+// due date was clicked 
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+  .text()
+  .trim();
 
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parents ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replacce("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with bootstrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+});
